46551129e60b270d2b0e95b26605f5c9ecc8de40
Sezimar Jr <sezimar_jrcs@hotmail.com>
2024-09-25 21:50:27 -0300

    feat: Criando banco de dados pela API da google books

--- livraria/admin.py
+++ livraria/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from livraria import models
 
 # Register your models here.
+
+
+@admin.register(models.Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = 'id', 'title', 'author', 'price', 'publish_date',
+    search_fields = 'title', 'author__name',
+    list_per_page = 20
+    ordering = '-id',
+    list_max_show_all = 200
+    list_display_links = 'id', 'title',
+
+
+@admin.register(models.Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    search_fields = 'name',
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    search_fields = 'name',
--- livraria/models.py
+++ livraria/models.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 #Criando o modelo de Autor
 class Author(models.Model):
-  name = models.CharField(max_length=200)
+  name = models.CharField(max_length=200,unique=True)
 
   def __str__(self) -> str:
     return self.name
@@ -14,7 +14,10 @@ class Author(models.Model):
 
 #Criando o modelo de Categoria
 class Category(models.Model):
-  name = models.CharField(max_length=200)
+  class Meta:
+    verbose_name = "Category"
+    verbose_name_plural = 'Categories'
+  name = models.CharField(max_length=200,unique=True)
 
   def __str__(self) -> str:
     return self.name
@@ -22,14 +25,17 @@ class Category(models.Model):
 
 #Criando o modelo de livros
 
-class Livro(models.Model):
+class Book(models.Model):
   title = models.CharField(max_length=200)
   author = models.ForeignKey(Author, on_delete=models.CASCADE) # Relacionamento com Autor
   category = models.ManyToManyField(Category, blank=True) # Relacionamento com Categoria (muitos-para-muitos)
-  price = models.DecimalField(max_length=6, decimal_places=2)
+  # isbn = models.CharField(max_length=13,unique=True)
+  price = models.DecimalField(max_digits=6, decimal_places=2)
   description = models.TextField()
-  publish_date = models.DateField()
+  publish_date = models.IntegerField(null=True,blank=True)
+  publisher = models.CharField(max_length=255,blank=True,null=True)
   pages = models.IntegerField(null=True,blank=True)
+  cover_url = models.URLField(max_length=200,null=True,blank=True)
 
   def __str__(self) -> str:
     return self.title
