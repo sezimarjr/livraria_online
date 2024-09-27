48093d7a8ff74dec6b2391ebb86365a694fafc0a
Sezimar Jr <sezimar_jrcs@hotmail.com>
2024-09-27 19:38:24 -0300

    feat: modelo carrinho

--- livraria/models.py
+++ livraria/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import User
 
 # Create your models here.
+
 
 
 #Criando o modelo de Autor
@@ -39,6 +41,28 @@ class Book(models.Model):
 
   def __str__(self) -> str:
     return self.title
+#Criando modelos carrinho
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"Carrinho de {self.user.username if self.user else 'Sessão Anônima'}"
 
+    @property
+    def total(self):
+        total = sum(item.subtotal for item in self.items.all())
+        return total
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.book.title} x {self.quantity}"
+
+    @property
+    def subtotal(self):
+        return self.book.price * self.quantity
 
